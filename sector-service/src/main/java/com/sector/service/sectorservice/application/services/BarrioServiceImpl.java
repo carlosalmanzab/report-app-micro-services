35490cfaa2f330bfef3fe496cdbb8b16d8ba76e8
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-01-05 17:34:25 -0500

    agregar java doc BarrioServiceImpl

--- sector-service/src/main/java/com/sector/service/sectorservice/application/services/BarrioServiceImpl.java
+++ sector-service/src/main/java/com/sector/service/sectorservice/application/services/BarrioServiceImpl.java
@@ -14,20 +14,37 @@ public class BarrioServiceImpl implements IBarrioService {
         this.barrioRepository = barrioRepository;
     }
 
+    /**
+     * Retrieves a Barrio object by its ID.
+     *
+     * @param id the ID of the Barrio object to retrieve
+     * @return an Optional containing the Barrio object if found, or an empty
+     *         Optional if not found
+     */
     @Override
     public Optional<Barrio> getById(Long id) {
         return barrioRepository.findById(id);
     }
 
+    /**
+     * Retrieves all the Barrio objects.
+     *
+     * @return a list of all the Barrio objects
+     */
     @Override
     public List<Barrio> getAll() {
         return barrioRepository.findAll();
     }
 
+    /**
+     * Retrieves a list of Barrio objects based on the given comuna ID.
+     *
+     * @param comunaId the ID of the comuna to filter the barrios by
+     * @return a list of Barrio objects that belong to the specified comuna
+     */
     @Override
     public List<Barrio> getByComunaId(Long comunaId) {
         return barrioRepository.findByComunaId(comunaId);
     }
-
 
 }
